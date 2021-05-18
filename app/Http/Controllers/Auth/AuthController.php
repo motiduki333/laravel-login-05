f41130d80af52385f83b2976361f06874480ad90
motiduki <masahumi3456>
2021-05-19 02:40:30 +0900

    account lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,7 +5,7 @@ use App\Http\Requests\LoginFormRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use App\Models\User;
 class AuthController extends Controller
 {
 
@@ -23,15 +23,51 @@ class AuthController extends Controller
     public function login(LoginFormRequest $request)
     {
         $credentials=$request->only('email','password');
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
 
-            return redirect()->route('home')->with('success','ログイン成功しました！');
+
+        $user=User::where('email','=',$credentials['email'])->first();
+        if (!is_null($user)){
+            
+            if($user->locked_flg===1){
+                
+                
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされています。',
+                    ]);
+                
+                
+
+            }
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                
+
+                if($user->error_count > 0){
+                //成功したらエラーカウント０にする
+                    $user->error_count=0;
+                  $user->save();
+                }
+                return redirect()->route('home')->with('success','ログイン成功しました！');
+            }
+            //ログインnに失敗したらエラーカウントを１増やす
+            $user->error_count=$user->error_count+1;
+
+            //エラーカウントが６以上の場合はアカウントロックする
+            if($user->error_count > 5){
+
+                $user->locked_flg=1;
+                $user->save();
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックさ荒れました。解除したい場合は運営者に連絡してください。',
+                    ]);
+            }
+            $user->save();
+            
         }
+
         return back()->withErrors([
             'danger' => 'メールアドレスか、パスワードが間違っています',
             ]);
-
     }
     /**
      * ユーザーをアプリケーションからログアウトさせる
